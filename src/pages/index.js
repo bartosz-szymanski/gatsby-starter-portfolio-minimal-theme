ed6c3b91798a1e782bfa4ac4956de66ecc01ee71
Bartosz Szymański <kontakt@bartoszszymanski.net>
2022-01-05 19:49:30 +0100

    Cennik added

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,11 +16,10 @@ export default function IndexPage() {
       <Seo title="MS TRANSLATIONS Milena Starzec" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        {/*<ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />*/}
         <AboutSection sectionId="about" heading="O mnie" />
         <InterestsSection sectionId="details" heading="Szczegóły" />
         <ProjectsSection sectionId="features" heading="Usługi" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ContactSection sectionId="github" heading="Inne" />
       </Page>
     </>
   );
